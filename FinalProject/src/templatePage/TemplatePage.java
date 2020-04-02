3060fce7e9f6c103368ccbfcbe73431e77df82f5
Korisnik <Korisnik@DESKTOP-2877K2G>
2020-04-03 01:44:08 +0200

    Home Page and Test

---
+++ FinalProject/src/templatePage/TemplatePage.java
@@ -0,0 +1,40 @@
+package templatePage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TemplatePage {
+
+	private WebDriver driver;
+	private Properties locators;
+	private WebDriverWait waiter;
+
+	public TemplatePage(WebDriver driver, Properties locators, WebDriverWait waiter) {
+		this.driver = driver;
+		this.locators = locators;
+		this.waiter = waiter;
+	}
+	
+	public void beforeClass() throws FileNotFoundException, IOException {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		this.locators =  new Properties();
+		locators.load(new FileInputStream("config/finalProject.properties"));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	
+	public void afterTest() {
+		this.driver.close();
+	}
+}
+	
+
