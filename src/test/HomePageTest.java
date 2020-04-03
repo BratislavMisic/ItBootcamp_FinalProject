f81f3c7ca7254ea6aab6e0e4d70aa274e4ce9bc7
BratislavMisic <61565786+BratislavMisic@users.noreply.github.com>
2020-04-03 04:00:48 +0200

    Add files via upload

---
+++ src/test/HomePageTest.java
@@ -0,0 +1,50 @@
+package test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import pages.HomePage;
+
+public class HomePageTest {
+
+	private WebDriver driver;
+	private Properties locators;
+	private WebDriverWait waiter;
+	
+	@BeforeClass
+	public void setup() throws FileNotFoundException, IOException {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		this.locators =  new Properties();
+		locators.load(new FileInputStream("config/finalProject.properties"));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	
+	}
+	
+	@Test
+	public void testHomePage() {
+		this.driver.navigate().to(this.locators.getProperty("main_url"));
+		HomePage homePage = new HomePage(driver, locators, waiter);
+		homePage.mainPageEntrance();
+		
+		Assert.assertTrue(homePage.mainPageValid());
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		this.driver.close();
+	}
+}
