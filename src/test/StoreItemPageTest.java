c357958b2866ed1ce2fb82f81c545c47389340d3
BratislavMisic <61565786+BratislavMisic@users.noreply.github.com>
2020-05-03 22:14:19 +0200

    Add files via upload

---
+++ src/test/StoreItemPageTest.java
@@ -0,0 +1,67 @@
+package test;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import pages.StoreItemPage;
+
+public class StoreItemPageTest {
+
+	private WebDriver driver;
+	private Properties locators;
+	private WebDriverWait waiter;
+
+	@Parameters("browser")
+	public void setup(String browser) throws Exception {
+		if(browser.equalsIgnoreCase("chrome") || browser.equalsIgnoreCase("google chrome")) {
+			System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+			this.driver = new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("firefox") || browser.equalsIgnoreCase("mozzila")) {
+			System.setProperty("webdriver.gecko.driver", "driver-lib\\geckodriver.exe");
+			this.driver = new FirefoxDriver();
+		}
+	    else{
+			//If no browser passed throw exception
+			throw new Exception("Browser is not correct");
+		}
+	}
+	
+	@BeforeClass
+	public void setup() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		this.locators =  new Properties();
+		locators.load(new FileInputStream("config/finalProject.properties"));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void storeItemTest() {
+		this.driver.navigate().to(this.locators.getProperty("item_url"));
+		
+		SoftAssert sa = new SoftAssert();
+		StoreItemPage sip = new StoreItemPage(driver, locators, waiter);
+		
+		sip.sendToCart();
+		sa.assertTrue(sip.sentToCart());
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		this.driver.close();
+	}
+}
